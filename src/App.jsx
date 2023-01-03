33a18d8f79f27cb654f434c1a3ce9c36f2ef0dc9
6reis <gracenunezr@gmail.com>
2023-01-03 12:16:26 -0300

    desafio-react

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+const userCredential = {
+    email: "",
+    password: "252525",
+};
+
+const App = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    // localStorage
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setError("");
+
+        if (!email.trim() || !password.trim()) {
+            return setError("Llena todos los campos");
+        }
+
+        if (
+            userCredential.email === email &&
+            userCredential.password === password
+        ) {
+            alert("Bienvenido");
+        } else {
+            setError("Credenciales no válidas");
+        }
+    };
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="text"
+                    placeholder="Ingrese email"
+                    name="email"
+                    value={email}
+                />
+                <input
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    placeholder="Ingrese contraseña"
+                    name="password"
+                    value={password}
+                />
+                {password === "252525" && <button type="submit">Submit</button>}
+            </form>
+        </>
+    );
+};
+
+export default App;
